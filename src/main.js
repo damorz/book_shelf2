aa1e82edd7b8da1956bf9d3d44aee242ccee97fd
damorz <dive2080@gmail.com>
2020-10-17 01:03:23 +0700

    add vue-router complete

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router"
+// import Vuetify from 'vuetify'
+// import Vuex from 'vuex';
+import routes from "./routes";
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+// Vue.component('HelloWorld', require('./components/HelloWorld.vue').default);
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes 
+});
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+let routes = [
+    {
+        path: "/",
+        name: "home",
+        component:  require("./components/home.vue").default
+    },
+    {
+        path: "/favorite",
+        name: "favorite",
+        component: require("./components/favorite.vue").default
+    },
+    {
+        path: "/book/:bookId",
+        name: "book",
+        component: require("./components/bookInfo.vue").default
+    }
+];
+
+export default routes;
