83d6d267099fec556e3ca3539b8a48709406fb5b
damorz <dive2080@gmail.com>
2020-10-17 01:12:37 +0700

    advuetify complete

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router"
+
 // import Vuetify from 'vuetify'
 // import Vuex from 'vuex';
 import routes from "./routes";
+
+import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter)
 
@@ -17,5 +20,6 @@ const router = new VueRouter({
 
 new Vue({
   render: h => h(App),
+  vuetify,
   router
 }).$mount('#app')
