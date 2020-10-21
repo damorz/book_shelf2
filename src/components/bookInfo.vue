f9929e537749c7dd8604ce51ff50caf2d479ecb3
damorz <dive2080@gmail.com>
2020-10-21 19:49:24 +0700

    fix main master

--- src/components/bookInfo.vue
+++ src/components/bookInfo.vue
@@ -1,9 +1,17 @@
 <template>
   <v-container v-if="hasBookInfo" class="fill-height" fluid>
-    <v-row class="book-info-container">
+    <v-row class="book-info-container" no-gutters>
+      <!-- Image Section -->
       <v-col cols="3">
         <v-img
-          v-if="hasThumbnail"
+          v-if="hasNoImage"
+          max-height="100%"
+          max-width="100%"
+          src="https://i.redd.it/s8lk86v3r2m11.png"
+        >
+        </v-img>
+        <v-img
+          v-else-if="hasThumbnail"
           max-height="100%"
           max-width="80%"
           class="ml-auto mr-auto"
@@ -19,6 +27,8 @@
         >
         </v-img>
       </v-col>
+
+      <!-- Title Section -->
       <v-col cols="5">
         <h1>{{ bookItem.volumeInfo.title }}</h1>
         <span v-if="hasAuthor">
@@ -32,31 +42,77 @@
           <h2 class="text-free" v-if="isFree" style="padding: 7% 0">
             FREE !
           </h2>
-          <h1 class="text-price" v-else style="padding: 7% 0">
+          <h1 class="text-price" v-else-if="!hasDiscount" style="padding: 7% 0">
             {{ bookItem.saleInfo.listPrice.amount }} &nbsp;
             {{ bookItem.saleInfo.listPrice.currencyCode }}
           </h1>
+          <h1
+            class="text-gray line-through-text"
+            v-if="hasDiscount"
+            style="padding: 7% 0 0 0"
+          >
+            {{ bookItem.saleInfo.listPrice.amount }} &nbsp;
+            {{ bookItem.saleInfo.listPrice.currencyCode }}
+          </h1>
+          <h1 class="text-error" style="padding: 0 0 7% 0" v-if="hasDiscount">
+            {{ bookItem.saleInfo.retailPrice.amount }}&nbsp;
+            {{ bookItem.saleInfo.retailPrice.currencyCode }}
+          </h1>
         </div>
         <div v-else class="price-block">
-            <h2 class="text-error" style="padding: 7% 0">
+          <h2 class="text-error" style="padding: 7% 0">
             Not avaliable for sale!
           </h2>
         </div>
-        <v-btn dark depressed color="blue">
+        <v-btn
+          id="buyBtn"
+          v-if="isForSale"
+          dark
+          depressed
+          color="blue"
+          @click="goToBuyPage"
+        >
           Buy this book
         </v-btn>
-        <v-btn
-          id="favoriteBtn"
-          class="btn-row-stack"
-          icon
-          @click="favoriteClick"
-        >
+
+        <v-btn v-if="this.favId != null" class="btn-row-stack" color="red" icon @click="favoriteClick">
           <v-icon>mdi-heart</v-icon>
         </v-btn>
+        <v-btn v-else class="btn-row-stack" icon @click="favoriteClick">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
         Add to favorite
       </v-col>
-      <v-col cols="4">
-        fill details
+      <v-col cols="4"> </v-col>
+    </v-row>
+
+    <!-- Detail Section -->
+    <v-row style="margin:1% 0" class="book-info-container">
+      <v-col cols="11" class="ml-auto mr-auto">
+        <!-- Description -->
+        <h3 class="description-title">Description :</h3>
+        <span
+          v-if="hasBookInfo"
+          v-html="bookItem.volumeInfo.description"
+        ></span>
+        <span v-if="!hasDescription">-</span>
+
+        <!-- Authors -->
+        <h3 class="description-title">Author :</h3>
+        <span v-for="author in bookItem.volumeInfo.authors" :key="author">
+          {{ author }} <span v-if="haveMoreThanOneAuthor">,</span>
+        </span>
+        <span v-if="!hasAuthor">-</span>
+
+        <!-- Page -->
+        <h3 class="description-title">Pages :</h3>
+        <span>{{ bookItem.volumeInfo.pageCount }} pages</span>
+
+        <!-- Publisher -->
+        <h3 class="description-title">Publisher :</h3>
+        <span>{{ bookItem.volumeInfo.publisher }}</span>
+        <span v-if="!hasPublisher">-</span>
       </v-col>
     </v-row>
   </v-container>
@@ -64,15 +120,43 @@
 
 <script>
 export default {
+  data() {
+    return {
+      favCount: null,
+      favId: null,
+      btnColor: true
+    };
+  },
   mounted() {
     this.$store.dispatch("searchBook", this.$route.params.bookId);
+    this.favId = localStorage.getItem(this.$route.params.bookId);
   },
   methods: {
+    goToBuyPage() {
+      window.open(this.bookItem.volumeInfo.infoLink);
+    },
     favoriteClick() {
-      if (this.isFavorite) {
-        //delete from localstorage
-      } else {
-        //add to localstorage
+      this.favCount = parseInt(localStorage.getItem("favoriteBookCount"));
+      if (this.favCount == null) {
+        this.favCount = 0;
+      }
+
+      this.favId = localStorage.getItem(this.bookItem.id);
+
+      //Already fav and remove
+      if(this.favId != null) {
+        localStorage.removeItem(this.bookItem.id);
+        localStorage.setItem("favoriteBookCount",this.favCount - 1 );
+        this.favCount --;
+        this.favId = null;
+      }
+
+      //Fav
+      else {
+        localStorage.setItem(this.bookItem.id,this.bookItem.id);
+        localStorage.setItem("favoriteBookCount",this.favCount + 1 );
+        this.favCount ++;
+        this.favId = localStorage.getItem(this.bookItem.id);
       }
     },
   },
@@ -101,8 +185,20 @@ export default {
     isFree() {
       return this.bookItem.saleInfo.listPrice.amount == 0;
     },
-    isFavorite() {
-      return true;
+    hasNoImage() {
+      return this.bookItem.volumeInfo.imageLinks == null;
+    },
+    hasDiscount() {
+      return (
+        this.bookItem.saleInfo.listPrice.amount >
+        this.bookItem.saleInfo.retailPrice.amount
+      );
+    },
+    hasDescription() {
+      return this.bookItem.volumeInfo.description != null;
+    },
+    hasPublisher() {
+      return this.bookItem.volumeInfo.publisher != null;
     },
   },
 };
