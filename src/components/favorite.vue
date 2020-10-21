77d816fb4f0277c92846c8afa4bdb24b7dbaaac1
damorz <dive2080@gmail.com>
2020-10-21 19:50:18 +0700

    fix main 2

--- src/components/favorite.vue
+++ src/components/favorite.vue
@@ -1,3 +1,19 @@
 <template>
     <h1>FAVORITE</h1>
 </template>
+<script>
+export default {
+    mounted() {
+        var archive = {}, // Notice change here
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+        while ( i-- ) {
+            if(keys[i] != "favoriteBookCount" && keys[i] != "loglevel:webpack-dev-server") {
+                archive[ keys[i] ] = localStorage.getItem( keys[i] );
+            }
+        }
+        console.log( archive);
+    }
+}
+</script>
