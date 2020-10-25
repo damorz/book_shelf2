77ace4daea6e25626945df8aada044f101d46f30
damorz <dive2080@gmail.com>
2020-10-26 02:01:24 +0700

    fix path CI/CD 5

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,22 +3,22 @@ let routes = [
   {
     path: "/",
     name: "Home",
-    component: require('../components/Home.vue'),
+    component: require("../components/Home").default,
   },
   {
     path: "/favorite",
     name: "Favorite",
-    component: require('../components/Favorite.vue'),
+    component: require("../components/Favorite").default,
   },
   {
     path: "/book/:bookId",
     name: "Book",
-    component: require('../components/BookInfo.vue'),
+    component: require("../components/BookInfo").default,
     props: true 
   },
   {
     path: "*",
-    component: require('../components/Home.vue'),
+    component: require("../components/Home").default,
   },
 ];
 export default routes;
